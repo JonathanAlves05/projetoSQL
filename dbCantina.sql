bd36fe4c3354f460cc30068cbec65e36320813a9
JonathanAlves05 <jarprodrigues05@gmail.com>
2024-08-09 20:22:35 -0300

    Projeto Cantina

--- dbCantina.sql
+++ dbCantina.sql
@@ -26,6 +26,13 @@ create table tbFornecedores(
     cnpj char(17) not null unique,
     primary key(codForn));
 
+create table tbClientes(
+    codCli int not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100),
+    telCelular char(10),
+    primary key(codCli));
+
 create table tbUsuario(
     codUsu int not null auto_increment,
     nome varchar(25) not null unique,
@@ -47,10 +54,25 @@ create table tbProdutos(
     foreign key(codForn) references tbFornecedores(codForn)
 );
 
+create table tbVendas(
+    codVenda int not null auto_increment,
+    dataVenda date,
+    horaVenda time,
+    quantidade int,
+    codUsu int not null,
+    codCli int not null,
+    codProd int not null,
+    primary key(codVenda),
+    foreign key(codUsu)references tbUsuario(codUsu),
+    foreign key(codCli)references tbClientes(codCli),
+    foreign key(codProd)references tbProdutos(codProd));
+
 
     -- visualizando a estrutura das tabelas
     desc tbFuncionarios;
+    desc tbClientes;
     desc tbUsuario; 
     desc tbFornecedores;
     desc tbProdutos;
+    desc tbVendas;
     
